48800dd4f15bce747614a67c04b59646ca02435e
Géraud Le Falher <daureg@gmail.com>
2009-06-29 18:15:07 +0200

    typo

--- config/info.mk
+++ config/info.mk
@@ -2,8 +2,7 @@ NAME			=	example
 FULL_TITLE		=	"Demonstration"
 DESC			=	"Un test d'une nouvelle"
 KEYWORD			=	"nouvelle,test"
-DATE            =   "today"
-
+DATE            	=   	"today"
 ROOT_LEVEL		=	"2"
 PDF_VIEWER		=	evince
 VERSION			=	0.0.1
