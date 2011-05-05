c7315f8593bd74e499df458067ba132d3c4d069b
Géraud Le Falher <daureg@gmail.com>
2011-05-05 18:27:18 +0200

    move a comment

--- nv2tex.sh
+++ nv2tex.sh
@@ -7,7 +7,6 @@ sed -i 's/\.\.\./\\dots ~ /' tmp
 sed -i 's/«/\\og /g' tmp
 sed -i 's/»/\\myfg/g' tmp
 sed -i 's/^-/\\tiret /' tmp
-#joindre les ligne \\
 sed -i 's/\*/\\emph\{/g' tmp
 sed -i 's/|\$/\}/g' tmp
 sed -i 's/^_1 /\\section\*\{/' tmp
@@ -16,4 +15,6 @@ sed -i 's/^_0 /\\chapter\{/' tmp
 sed -i '/^\\chapter/ s/$/\}/' tmp
 sed -i 's/^_2 /\\subsection\*\{/' tmp
 sed -i '/^\\subsection\*/ s/$/\}/' tmp
+sed -i 's/%/\\%/' tmp
+#joindre les ligne \\
 sed -e :a -e '$!N;s/\n_/\\\\/;ta' -e 'P;D' tmp > texte.tex
